02bfb447501852121694d2b951ca6d21a0795490
maciekbartczak <maciekbartczak17@gmail.com>
2021-10-30 14:04:38 +0200

    added class for http status codes

---
+++ backend/app/lib/HttpStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+class HTTP_STATUS {
+    public static int $OK = 200;
+    public static int $CREATED = 201;
+    public static int $BAD_REQUEST = 400;
+    public static int $UNATHORIZED = 401;
+    public static int $FORBIDDEN = 403;
+    public static int $NOT_FOUND = 404;
+    public static int $METHOD_NOT_ALLOWED = 405;
+    public static int $UNPROCESSABLE_ENTITY = 422;
+    public static int $SERVER_ERROR = 500;
+}
