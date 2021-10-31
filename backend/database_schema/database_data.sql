2c03da5e25603d1882c55290bd249ee8bb90c50b
maciekbartczak <maciekbartczak17@gmail.com>
2021-10-31 22:55:38 +0100

    create readme & add predefined users

--- backend/database_schema/database_data.sql
+++ backend/database_schema/database_data.sql
@@ -3,3 +3,7 @@ USE iab;
 INSERT INTO user_role (role) VALUE ('USER');
 INSERT INTO user_role (role) VALUE ('EMPLOYEE');
 INSERT INTO user_role (role) VALUE ('ADMIN');
+
+INSERT INTO user (role_id, username, password) VALUES (1, 'teryD', 'teryD');
+INSERT INTO user (role_id, username, password) VALUES (2, 'adam', 'adam');
+INSERT INTO user (role_id, username, password) VALUES (3, 'admin', 'admin');
