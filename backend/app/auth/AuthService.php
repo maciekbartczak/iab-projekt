4c20d3c5b0c6195c4e3dd907bfe8f1dfe2a5ab15
maciekbartczak <maciekbartczak17@gmail.com>
2021-11-04 12:51:47 +0100

    backend - refactor user authorization

---
+++ backend/app/auth/AuthService.php
@@ -0,0 +1,26 @@
+<?php
+
+class AuthService {
+
+
+    /**
+     * @param $user_id user id that is checked against user id extracted from token
+     * @return bool true if token is valid and id's match, false otherwise
+     */
+    public static function authorizeUserFromToken($user_id): bool {
+        $res = new Response();
+        try {
+            $token_data = JwtUtils::getDecodedJWT();
+        } catch (Exception $e) {
+            $res->status(HTTP_STATUS::UNATHORIZED)->send();
+            return false;
+        }
+
+        $auth_user_id = $token_data->userDetails->id;
+        if ($auth_user_id != $user_id) {
+            $res->status(HTTP_STATUS::FORBIDDEN)->send();
+            return false;
+        }
+        return true;
+    }
+}
